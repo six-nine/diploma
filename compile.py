2ed3fd07db0357b181d592099a124ee50c2dc1eb
Vadzim Kazlou <vadickozlov@gmail.com>
2024-05-27 02:49:12 +0300

    Some stp fizes

--- compile.py
+++ compile.py
@@ -25,7 +25,7 @@ def main():
     clean_cmd = 'make -C "src" clean'
     
     docker_platform_flag = '--platform linux/amd64' if sysconfig.get_platform().split("-")[-1].lower() == 'arm64' else ''
-    run_docker_cmd = f'docker run {docker_platform_flag} -i --rm -v "{os.getcwd()}:/test" -w /test ghcr.io/fcsan-bsuir/bsuir_tex:main'
+    run_docker_cmd = f'docker run {docker_platform_flag} -i --rm -v "{os.getcwd()}:/test" -w /test ajiob/docker-xelatex-fonts:1.2.1'
 
     shell_and_symbol = ";" if system == "Windows" else "&&"
 
