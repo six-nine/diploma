50d2b2a1f04e429336aab0730d2107eb44117e8c
Vadzim Kazlou <vadickozlov@gmail.com>
2024-06-02 12:45:57 +0300

    Final version

--- compile.py
+++ compile.py
@@ -26,6 +26,7 @@ def main():
     
     docker_platform_flag = '--platform linux/amd64' if sysconfig.get_platform().split("-")[-1].lower() == 'arm64' else ''
     run_docker_cmd = f'docker run {docker_platform_flag} -i --rm -v "{os.getcwd()}:/test" -w /test ajiob/docker-xelatex-fonts:1.2.1'
+    #run_docker_cmd = f'docker run {docker_platform_flag} -i --rm -v "{os.getcwd()}:/test" -w /test ghcr.io/fcsan-bsuir/bsuir_tex:main'
 
     shell_and_symbol = ";" if system == "Windows" else "&&"
 
